3c6371f8b11eada41e2641a4bece1cf4cd25365c
jhoogmoed <28733969+jhoogmoed@users.noreply.github.com>
2020-12-15 18:31:22 +0100

    fix import issues

--- htpmKitti/main.py
+++ htpmKitti/main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-# import kitti
+import kitti
 import model.services as m
-# import online
+import online
 
 kp = m.kitti_parser()
 x = [0., 1.458974, 2.63547244, 0.96564807, 2.21222542, 1.65225034, 0., 0., 1.,
